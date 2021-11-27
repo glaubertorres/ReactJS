725d406cca5f3ad13222571c409180d1183309d5
glaubertorres <glauberthierryt@gmail.com>
2021-11-27 10:48:20 -0300

    exercício de react, blog pessoal

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className="title">Home</h1>
+            <img src="https://i.imgur.com/z54AgWQ.gif" alt="" className="gif" />
+        </>
+    );
+}
+
+export default Home;
