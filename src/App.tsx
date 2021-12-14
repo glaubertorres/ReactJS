c08b10d11968bdd59f0b1162c73191e4d9927a08
glaubertorres <glauberthierryt@gmail.com>
2021-12-13 21:29:31 -0300

    blog pessoal - 13/12

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,79 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Navbar from './components/static/navbar/Navbar';
-import Footer from './components/static/footer/Footer';
-import UserRegistration from './pages/userRegistration/UserRegistration';
-import Login from './pages/login/Login';
-import {Grid} from '@material-ui/core';
-import Home from './pages/home/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
+import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
 import './App.css';
+import ListaTema from './components/temas/listatema/ListaTema';
+import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
+import CadastroPost from './components/postagens/cadastroPost/CadastroPost';
+import CadastroTema from './components/temas/cadastroTema/CadastroTema';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
+import {Provider} from 'react-redux';
+import store from './store/store';
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
   return (
+    <Provider store={store}>
+      <ToastContainer />
     <Router>
       <Navbar />
-        <Switch>
-          <div>
-            <Route exact path='/'>
-              <Login />
-            </Route>
-            <Route path='/login'>
-              <Login />
-            </Route>
-            <Route path='/home'>
-              <Home />
-            </Route>
-            <Route path='/userregistration'>
-              <UserRegistration />
-            </Route>
-          </div>
-        </Switch>
+      <Switch>
+        <div style={{ minHeight: '100vh' }}>
+
+          <Route exact path='/'>
+            <Login />
+          </Route>
+
+          <Route path='/login'>
+            <Login />
+          </Route>
+
+          <Route path='/home'>
+            <Home />
+          </Route>
+
+          <Route path='/cadastrousuario'>
+            <CadastroUsuario />
+          </Route>
+          <Route path='/temas'>
+            <ListaTema />
+          </Route>
+          <Route path='/posts'>
+            <ListaPostagem />
+          </Route>
+
+          <Route exact path='/formularioPostagem'>
+            <CadastroPost />
+          </Route>
+          <Route exact path='/formularioPostagem/:id'>
+            <CadastroPost />
+          </Route>
+          <Route exact path='/formularioTema'>
+            <CadastroTema />
+          </Route>
+          <Route exact path='/formularioTema/:id'>
+            <CadastroTema />
+          </Route>
+          <Route path='/deletarPostagem/:id'>
+            <DeletarPostagem />
+          </Route>
+          <Route path='/deletarTema/:id'>
+            <DeletarTema />
+          </Route>
+
+
+        </div>
+      </Switch>
       <Footer />
     </Router>
+    </Provider>
   );
 }
 
